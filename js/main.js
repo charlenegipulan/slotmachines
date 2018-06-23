1079caf58f9095946d358472adb7de0e6e201224
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-06-22 22:22:20 -0700

    add basic markup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 /*----- constants -----*/
 
 
+
 /*----- app's state (variables) -----*/
+
+var reels, score;
+
+
 
 
 /*----- cached element references -----*/
+var scoreEl = document.querySelector('h1');
+
 
 
 /*----- event listeners -----*/
+document.getElementById('inc-btn').addEventListener('click', function() {
+    handleUpdateScore(1);
+});
+document.getElementById('dec-btn').addEventListener('click', function() {
+    handleUpdateScore(-1);
+});
+
+
+
 
 
 /*----- functions -----*/
+function initialize() {
+    reels= [null, null, null];
+    score = 0;
+
+    render();
+}
+
+
+//transfers all state to the DOM(Visualization)
+function render() {
+    scoreEl.textContent = score;
+}
+
+
+function handleUpdateScore(diff) {
+    score += diff;
+
+    render();
+}
+
+
+initialize();
