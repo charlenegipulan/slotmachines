9aea2c9a62d6fdda6fb407a94761c9ceb6ceeed3
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-06-23 01:24:09 -0700

    initial css styling

--- js/main.js
+++ js/main.js
@@ -4,48 +4,54 @@
 
 /*----- app's state (variables) -----*/
 
-var reels, score;
+// var reels, score, betAmount, currentWinnings, 
 
 
 
 
 /*----- cached element references -----*/
-var scoreEl = document.querySelector('h1');
+// var scoreEl = document.querySelector('h1');
 
 
 
 /*----- event listeners -----*/
-document.getElementById('inc-btn').addEventListener('click', function() {
-    handleUpdateScore(1);
-});
-document.getElementById('dec-btn').addEventListener('click', function() {
-    handleUpdateScore(-1);
-});
+// document.getElementById('inc-btn').addEventListener('click', function() {
+//     handleUpdateScore(1);
+// });
+// document.getElementById('dec-btn').addEventListener('click', function() {
+//     handleUpdateScore(-1);
+// });
 
 
 
 
 
 /*----- functions -----*/
+
+// var bet = prompt("Bet amount: ");
+// console.log(bet);
+// document.getElementById('bettedAmount').innerHTML = bet;
+
 function initialize() {
     reels= [null, null, null];
     score = 0;
 
-    render();
+     render();
 }
+
 
 
 //transfers all state to the DOM(Visualization)
-function render() {
-    scoreEl.textContent = score;
-}
+// function render() {
+//     scoreEl.textContent = score;
+// }
 
 
-function handleUpdateScore(diff) {
-    score += diff;
+// function handleUpdateScore(diff) {
+//     score += diff;
 
-    render();
-}
+//     render();
+// }
 
 
-initialize();
+// initialize();
